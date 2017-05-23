b1fc393f3d35d4cefaa44acc41d0bddba6ce2529
chong yang <chongyang@JustinMac.local>
2017-05-22 23:10:13 -0400

    test the added tab

--- src/app/post/post.routes.ts
+++ src/app/post/post.routes.ts
@@ -3,5 +3,5 @@ import { Routes } from '@angular/router';
 import { PostComponent } from './post.component';
 
 export const PostRoutes: Routes = [
-  { path: '',  component: PostComponent }
+  { path: 'post',  component: PostComponent }
 ];
